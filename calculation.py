00a445207d8292f87bdd5788cbd14ccc8aa09421
theantran372000 <trantheanh.ktmt@gmail.com>
2022-05-06 03:33:56 +0000

    feat(calculation) add rectangle area function

--- calculation.py
+++ calculation.py
@@ -6,11 +6,15 @@ def cal_rectangle_perimeter(a, b):
 def cal_circle_area(r):
     return math.pi * math.pow(r, 2)
 
+def cal_rectangle_area(a, b):
+    return a * b
+
 if __name__ == '__main__':
     f = int (input('''
         Choose your function:
             0. Calculate rectangle perimerter
             1. Calculate circle area
+            2. Calculate rectangle area
             
         Your choice: '''))
     
@@ -27,4 +31,14 @@ if __name__ == '__main__':
         
         result = cal_circle_area(r)
     
+<<<<<<< Updated upstream
     print('Result: {}'.format(result * mul))
+=======
+    elif f == 2:
+        a = int(input("Input a = "))
+        b = int(input("Input b = "))
+        
+        result = cal_rectangle_area(a, b)
+        
+    print('Result: {}'.format(result))
+>>>>>>> Stashed changes
